2e77ef7ee937d3d1b464224a773e5db84a12e3c6
冯不游 <71683364+mefengl@users.noreply.github.com>
2023-08-08 15:05:25 +0800

    feat: Add chat_check.sh script for calling OpenAI API for chat completions

---
+++ chat_check.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+API_KEYS=$1
+IFS=',' read -ra API_KEY_ARRAY <<<"$API_KEYS"
+
+URL="https://api.openai.com/v1/chat/completions"
+CONTENT_TYPE="Content-Type: application/json"
+
+DATA='{
+  "model": "gpt-3.5-turbo",
+  "messages": [
+    {
+      "role": "user",
+      "content": "1+1=x, x="
+    }
+  ]
+}'
+
+for i in "${!API_KEY_ARRAY[@]}"; do
+	API_KEY=${API_KEY_ARRAY[i]}
+	AUTHORIZATION="Authorization: Bearer $API_KEY"
+
+	success=false
+	for attempt in {1..2}; do
+		RESPONSE=$(curl -s -X POST -H "$CONTENT_TYPE" -H "$AUTHORIZATION" -d "$DATA" $URL)
+		ERROR_CODE=$(echo $RESPONSE | jq -r '.error.code')
+
+		if [ "$ERROR_CODE" = "null" ]; then
+			success=true
+			break
+		fi
+	done
+
+	if [ "$success" = false ]; then
+		echo "Request failed with key: $API_KEY"
+		echo "Error code: $ERROR_CODE"
+	fi
+done
